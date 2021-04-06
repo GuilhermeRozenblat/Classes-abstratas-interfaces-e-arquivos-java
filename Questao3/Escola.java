c0ca01ba5ffcd0f452881c5290d182bfc4675787
GuilhermeRozenblat <79062840+GuilhermeRozenblat@users.noreply.github.com>
2021-04-06 18:11:07 -0300

    Add files via upload

---
+++ Questao3/Escola.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Questao3;
+
+/**
+ *
+ * @author Guilherme
+ */
+public class Escola extends Edificio {
+
+    public Escola(int numPessoas, boolean energiaRenovavel, int numLampadas, boolean arcondicionado, int horasDiarias) {
+        super(numPessoas, energiaRenovavel, numLampadas, arcondicionado, horasDiarias);
+    }
+    
+}
